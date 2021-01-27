31f8e6dcdeffec2664f18cf121a0413be7fc7540
t <ruel@stu.usc.edu.tt>
2021-01-27 18:06:10 -0400

    message

--- src/Question1.java
+++ src/Question1.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
 
 public class Question1 {
-}
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+    }//end of main
+}//end of class
