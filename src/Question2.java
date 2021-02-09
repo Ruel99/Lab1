992262fcbc60e1c03f0e71768a512bd3b525c89e
t <ruel@stu.usc.edu.tt>
2021-02-09 10:17:23 -0400

    message

--- src/Question2.java
+++ src/Question2.java
@@ -10,20 +10,26 @@ public class Question2 {
 
         System.out.println("Welcome to my guessing game where the objective is to guess the secret number. " +
                 "In this game you have 5 lives, so have fun and good luck");
-        for(int i = 0; i<=5; i++){
+    int i = 1;
+        while (i <=5){
             System.out.println("please enter a number");
             int userInput = sc.nextInt();
 
             if(userInput == secNum){
                 System.out.println("You won, the secret number was " + secNum);
+                break;
             }else if (userInput < secNum) {
                 System.out.println("Your guess is too low. Try again");
             }else if(userInput > 10){
                 System.out.println("The secret number is between 0 and 10");
-            }else {
-                System.out.println("your guess is too high. Try again");
-            }//end of if else statement
-        }//end of for loop
-        System.out.println("You ran out of lives. The secret number was "+ secNum);
+            }else if(i == 5) {
+                System.out.println("You ran out of lives. The secret number was "+ secNum);
+            }else{
+                System.out.println("Your guess is too high. Try again");
+            }
+            i++;
+
+        }//end of while loop
+
     }//end of main
 }//end of class
